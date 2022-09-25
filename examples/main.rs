931baa8d254ffbf80adf64108daba46fbcf810d2
Matt Fellenz <matt@felle.nz>
2022-09-24 18:59:46 -0700

    Refactor CLI
    - Use argh for argument parsing.
    - Allow showing any combination of the steps of the pipeline rather than only one. So for example you can see the decomposed words and lexed tokens at the same time.
    - Improve the printed representations somewhat. Anywhere `Span`s are printed, print the actual input for that span as well.
    - Add my standard lint configuration.

--- examples/main.rs
+++ examples/main.rs
@@ -1,48 +1,62 @@
+#![deny(
+	absolute_paths_not_starting_with_crate,
+	elided_lifetimes_in_paths,
+	explicit_outlives_requirements,
+	future_incompatible,
+	keyword_idents,
+	macro_use_extern_crate,
+	meta_variable_misuse,
+	missing_abi,
+	non_ascii_idents,
+	nonstandard_style,
+	noop_method_call,
+	pointer_structural_match,
+	private_in_public,
+	rust_2018_idioms
+)]
+#![warn(clippy::pedantic, unused_qualifications)]
+#![forbid(unsafe_code)]
+
 use std::io::Read as _;
 
+/// Interact with sneturfahi via the command line.
+#[derive(argh::FromArgs, Debug)]
+#[allow(clippy::struct_excessive_bools)] // CLI arguments
+struct Args {
+	/// enable verbose logging
+	#[argh(switch)]
+	verbose: bool,
+	/// print the decomposed words
+	#[argh(switch)]
+	decompose: bool,
+	/// print the lexed tokens
+	#[argh(switch)]
+	lex: bool,
+	/// print the CST
+	#[argh(switch)]
+	cst: bool,
+}
+
 fn main() {
-	if std::env::var_os("VERBOSE").is_some() {
+	let args: Args = argh::from_env();
+
+	if args.verbose {
 		simplelog::SimpleLogger::init(log::LevelFilter::Trace, simplelog::Config::default()).unwrap();
 	}
 
-	let action = std::env::args().nth(1).expect("need action");
-	match action.as_str() {
-		"cst" => parse_cst(),
-		"lex" => lex(),
-		"decompose" => decompose(),
-		_ => panic!("unknown action"),
+	if !(args.decompose || args.lex || args.cst) {
+		eprintln!("At least one of `--decompose`, `--lex`, `--cst` is required.");
+		return;
 	}
-}
 
-fn repl(mut callback: impl FnMut(&str)) {
-	let mut input = String::new();
-	if atty::is(atty::Stream::Stdin) {
-		loop {
-			eprint!("> ");
-			input.clear();
-			if std::io::stdin().read_line(&mut input).unwrap() == 0 {
-				break;
-			}
-			callback(input.trim());
+	repl(move |input| {
+		if args.decompose {
+			let decomposed = sneturfahi::decompose(input);
+			println!("Decomposed: {:?}", DebugWithIterator(decomposed, input));
 		}
-	} else {
-		std::io::stdin().read_to_string(&mut input).unwrap();
-		callback(input.trim());
-	}
-}
 
-fn decompose() {
-	repl(|input| {
-		let decomposed: Vec<_> = sneturfahi::decompose(input)
-			.map(|span| span.slice(input).unwrap())
-			.collect();
-		println!("Decomposed: {decomposed:?}");
-	});
-}
-
-fn parse_cst() {
-	repl(|input| {
 		let lexed: Result<Vec<_>, _> = sneturfahi::lex(input).collect();
+
 		let lexed = match lexed {
 			Ok(lexed) => lexed,
 			Err(error) => {
@@ -50,16 +64,80 @@ fn parse_cst() {
 				return;
 			}
 		};
-		match sneturfahi::Cst::parse(&lexed) {
-			Ok(cst) => println!("CST: {:#?}", cst.root()),
-			Err(error) => println!("Error: {error:?}"),
+
+		if args.lex {
+			println!(
+				"Lexed: {:?}",
+				DebugWithIterator(lexed.iter().copied(), input)
+			);
 		}
-	})
+
+		if args.cst {
+			match sneturfahi::Cst::parse(&lexed) {
+				Ok(cst) => println!("CST: {:#?}", cst.root()),
+				Err(error) => println!("Error: {error:?}"),
+			}
+		}
+	});
 }
 
-fn lex() {
-	repl(|input| {
-		let lexed: Result<Vec<_>, _> = sneturfahi::lex(input).collect();
-		println!("Lexed: {lexed:?}");
-	});
+#[derive(Clone, Copy)]
+struct WithInput<'input, T>(T, &'input str);
+
+impl std::fmt::Debug for WithInput<'_, sneturfahi::Span> {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			formatter,
+			"{:?} @ {:?}",
+			self.0.slice(self.1).unwrap(),
+			self.0.start..self.0.end
+		)
+	}
+}
+
+impl std::fmt::Debug for WithInput<'_, sneturfahi::lex::Token> {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			formatter,
+			"{}{}({:?})",
+			self.0.selmaho.as_repr(),
+			if self.0.experimental { "*" } else { "" },
+			WithInput(self.0.span, self.1)
+		)
+	}
+}
+
+struct DebugWithIterator<'input, T>(T, &'input str);
+
+impl<'input, T> std::fmt::Debug for DebugWithIterator<'input, T>
+where
+	T: Iterator + Clone,
+	WithInput<'input, <T as Iterator>::Item>: std::fmt::Debug,
+{
+	fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		formatter
+			.debug_list()
+			.entries(self.0.clone().map(move |item| WithInput(item, self.1)))
+			.finish()
+	}
+}
+
+fn repl(mut callback: impl FnMut(&str)) {
+	use std::io::BufRead as _;
+
+	let mut input = String::new();
+	let mut stdin = std::io::stdin().lock();
+	if atty::is(atty::Stream::Stdin) {
+		loop {
+			eprint!("> ");
+			input.clear();
+			if stdin.read_line(&mut input).unwrap() == 0 {
+				break;
+			}
+			callback(input.trim());
+		}
+	} else {
+		stdin.read_to_string(&mut input).unwrap();
+		callback(input.trim());
+	}
 }
